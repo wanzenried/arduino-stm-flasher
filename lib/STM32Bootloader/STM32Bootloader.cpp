4b91543fdfb429a99a0289cb44791b2a886bfd62
wanzenried <62063187+wanzenried@users.noreply.github.com>
2025-07-24 19:20:05 +0200

    moved helper functions to class

--- lib/STM32Bootloader/STM32Bootloader.cpp
+++ lib/STM32Bootloader/STM32Bootloader.cpp
@@ -5,3 +5,59 @@ explicit STM32Bootloader::STM32Bootloader(I2C_Interface& I2C)
 {
 
 }
+
+uint8_t STM32Bootloader::bytes_checksum(uint8_t* bytes, size_t amount)
+{
+    uint8_t sum = 0x00;
+    for (size_t i = 0; i < amount; i++)
+    {
+        sum ^= bytes[i];
+    }
+    return sum;
+}
+
+// len can be no more than 32 because of Wire.h restrictions
+int8_t STM32Bootloader::send_frame(uint8_t* tx_buf, size_t len)
+{
+    if (len > 32)
+    {
+        return -1;  // too much data
+    }
+    !I2C.beginTransmission(_I2C_addr);
+    I2C.writeBytes(tx_buf, len);
+    return I2C.endTransmission();
+}
+
+int8_t STM32Bootloader::send_cmd(uint8_t cmd)
+{
+    _tx_buf[0] = cmd;
+    _tx_buf[1] = ~cmd;
+    return send_frame(_tx_buf, 2);
+}
+
+int8_t STM32Bootloader::send_address(uint32_t address)
+{
+    _tx_buf[0] = (address >> 24) & 0xFF;
+    _tx_buf[1] = (address >> 16) & 0xFF;
+    _tx_buf[2] = (address >> 8) & 0xFF;
+    _tx_buf[3] = address & 0xFF;
+    _tx_buf[4] = bytes_checksum(_tx_buf, 4);
+    return send_frame(_tx_buf, 5);
+}
+
+// len can be no more than 30 because of Wire.h restrictions [len + bytes + checksum]
+int8_t STM32Bootloader::send_data(uint8_t* data, size_t len)
+{
+    if (len > 30)
+    {
+        return -1;  // too much data
+    }
+    _tx_buf[0] = uint8_t(len - 1);  // STM32 expects N+1 bytes
+    for (size_t i = 0; i < len; i++)
+    {
+        _tx_buf[i+1] = data[i];
+    }
+    _tx_buf[len+1] = bytes_checksum(_tx_buf, len+1);
+
+    return send_frame(_tx_buf, len+2);    
+}
--- lib/STM32Bootloader/STM32Bootloader.hpp
+++ lib/STM32Bootloader/STM32Bootloader.hpp
@@ -9,11 +9,21 @@ class STM32Bootloader
 private:
     I2C_Interface& I2C;
     uint8_t _I2C_addr = cfg::STM32_I2C_ADDR;
+    uint8_t _tx_buf[32];
 public:
     explicit STM32Bootloader(I2C_Interface& I2C);
 
     void set_I2C_addr(uint8_t I2C_addr) {_I2C_addr = I2C_addr;}
-    uint8_t get_I2C_addr(void) const {return _I2C_addr;}
+    uint8_t get_I2C_addr() const {return _I2C_addr;}
+
+    // helper functions
+    uint8_t bytes_checksum(uint8_t* bytes, size_t amount);
+    int8_t send_frame(uint8_t *tx_buf, size_t len);
+    int8_t send_cmd(uint8_t cmd);
+    int8_t send_address(uint32_t address);
+    int8_t send_data(uint8_t* data, size_t len);
+
+    bool wait_ack(uint64_t timeout_ms);
 
 };
 
