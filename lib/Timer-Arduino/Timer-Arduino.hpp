276baecfb7540d4638520ed84ab790d78bdb25dc
wanzenried <62063187+wanzenried@users.noreply.github.com>
2025-08-06 16:28:36 +0200

    added initialization of bootloader in main program

--- lib/Timer-Arduino/Timer-Arduino.hpp
+++ lib/Timer-Arduino/Timer-Arduino.hpp
@@ -7,6 +7,8 @@ class ArduinoTimer : public ITimer
 {
 
 public:
+    ArduinoTimer() {}
+
     uint32_t millis() override {return ::millis();}
     void delay(uint32_t ms) override {::delay(ms);}
 };
